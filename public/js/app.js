021f67896d41d9c52ff9f89fcb966097496f65b3
Ingrid Carlson <ingridclaire@gmail.com>
2021-04-13 10:28:32 -0400

    adds tweet.js file for functions related to adding tweets

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 const App = {
+  
   baseUrl: 'https://the-tweeter-api.herokuapp.com',
+
   username: 'anonymous',
+
   initialize: function() {
     //call all functions that are needed when the app first loads here
   },
+
+  setUsername: function() {
+    //write logic here for getting and setting the current username
+  },
+
   fetchTweets: function () {
     //use axios to GET all tweets from the datase at the endpoint '/tweets'
     //after getting tweets, decide what to do with the data
-  }
+  },
+
 }
---
+++ public/js/tweet.js
@@ -0,0 +1,8 @@
+const Tweet = {
+  createTweet: function(/*add param if necessary*/) {
+    //write code here for creating a new tweet (used to dynamically create html content)
+  },
+  renderTweets: function(/*add param if necessary*/) {
+    //write code here for adding tweet data to the DOM
+  }
+}
