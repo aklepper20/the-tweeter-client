645e8fd49ec94ff12b10d3a03d98208f3498fe95
Ingrid Carlson <ingridclaire@gmail.com>
2021-04-13 10:20:42 -0400

    sets up Apps.js skeleton for building out needed functions in OOP style

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+const App = {
+  baseUrl: 'https://the-tweeter-api.herokuapp.com',
+  username: 'anonymous',
+  initialize: function() {
+    //call all functions that are needed when the app first loads here
+  },
+  fetchTweets: function () {
+    //use axios to GET all tweets from the datase at the endpoint '/tweets'
+    //after getting tweets, decide what to do with the data
+  }
+}
