903bcb8bdfb756c4cbe8756a627a7bc1d87aee98
Aly Klepper <alyklepper1@gmail.com>
2021-04-13 12:01:18 -0700

    Ip

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,36 @@
 const App = {
-  
+
   baseUrl: 'https://the-tweeter-api.herokuapp.com',
 
   username: 'anonymous',
 
   initialize: function() {
     //call all functions that are needed when the app first loads here
+    this.setUsername();
+    this.fetchTweets();
   },
 
   setUsername: function() {
     //write logic here for getting and setting the current username
+    let userBrowser = prompt("What's your user name?");
+    this.username = userBrowser;
   },
 
   fetchTweets: function () {
+    console.log('fetch tweets')
     //use axios to GET all tweets from the datase at the endpoint '/tweets'
     //after getting tweets, decide what to do with the data
+    axios.get(`${this.baseUrl}/tweets`)
+     .then(function (response) {
+        // handle success
+       Tweet.renderTweets(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      console.log('sent axios request')
   },
 
 }
--- public/js/tweet.js
+++ public/js/tweet.js
@@ -1,8 +1,19 @@
 const Tweet = {
-  createTweet: function(/*add param if necessary*/) {
+  tweetsContainer: document.querySelector('.tweets-container'),
+  createTweet: function(tweetData) {
     //write code here for creating a new tweet (used to dynamically create html content)
+    return `<div>
+      <h4>${tweetData.username}</h4>
+      <p>${tweetData.message}</p>
+    </div>`
   },
-  renderTweets: function(/*add param if necessary*/) {
+  renderTweets: function(tweetsArray) {
     //write code here for adding tweet data to the DOM
+    let htmlString = '';
+    for (let i = 0; i < tweetsArray.length; i++) {
+      const newTweet = this.createTweet(tweetsArray[i]);
+      htmlString += newTweet;
+    }
+    this.tweetsContainer.innerHTML = htmlString;
   }
 }
